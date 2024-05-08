cc08abbcab7b55ffe2b723fc3176a2dcd7f46bdd
Nam Nguyễn Bá <102220286@sv1.dut.udn.vn>
2024-05-08 16:16:19 +0700

    add databse basic

---
+++ SQLQuery.sql
@@ -0,0 +1,65 @@
+create database QuanLyQuanCafe
+go
+
+USE QuanLyQuanCafe
+GO
+
+CREATE TABLE TableFood
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'Bàn không có tên',
+	status NVARCHAR(100) NOT NULL DEFAULT N'Trống'
+)
+GO
+
+CREATE TABLE Account
+(
+	UserName NVARCHAR(100) PRIMARY KEY,
+	DisplayName NVARCHAR(100) NOT NULL DEFAULT N'Shiro',
+	PassWord NVARCHAR(1000) NOT NULL DEFAULT 0,
+	Type INT NOT NULL DEFAULT 0
+
+)
+GO
+
+CREATE TABLE FoodCategory
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'Không có tên'
+)
+GO
+
+CREATE TABLE Food
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'Không có tên',
+	idCategory INT NOT NULL,
+	price FLOAT NOT NULL DEFAULT 0
+
+	FOREIGN KEY (idCategory) REFERENCES dbo.FoodCategory(id)
+)
+GO
+
+CREATE TABLE Bill
+(
+	id INT IDENTITY PRIMARY KEY,
+	DateCheckIn DATE NOT NULL DEFAULT GETDATE(),
+	DateCheckOut DATE,
+	idTable INT NOT NULL,
+	status INT NOT NULL DEFAULT 0
+
+	FOREIGN KEY (idTable) REFERENCES dbo.TableFood(id)
+)
+GO
+
+CREATE TABLE BillInfo
+(
+	id INT IDENTITY PRIMARY KEY,
+	idBill INT NOT NULL,
+	idFood INT NOT NULL,
+	count INT NOT NULL DEFAULT 0
+
+	FOREIGN KEY (idBill) REFERENCES dbo.Bill(id),
+	FOREIGN KEY (idFood) REFERENCES dbo.Food(id)
+)
+
